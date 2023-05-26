2a952c3bf99746d44f362248a1d0d44205830eaa
AlexandraD <alexa929292@gmail.com>
2023-05-26 14:17:45 +0200

    NewEntry

--- Ejercicio4/Program.cs
+++ Ejercicio4/Program.cs
@@ -10,6 +10,19 @@ namespace Ejercicio4
     {
         static void Main(string[] args)
         {
+            /*
+             Recorre los números del 1 al 100. Usa un bucle for.
+             */
+            Console.WriteLine("Los números del 1 al 100 son : ");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            for (int i = 1; i < 101; i++) {
+                Console.WriteLine($"Numero: {i}");
+                    }
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Ya se han recorido los 100 primeros números");
+            Console.ReadKey();
         }
     }
 }
---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             Recorre los números del 1 al 100. Muestra los números pares. Usar el tipo de bucle que quieras.
+             */
+            Console.WriteLine("Los números pares del 1 al 100.");
+            Console.WriteLine(""); 
+            Console.WriteLine("");
+            //OPCIÓN 1
+            //for (int i = 1; i < 101; i++)
+            //{
+            //    if((i % 2) == 0)
+            //    {
+            //        Console.WriteLine($"{i}");
+            //    }
+
+            //}
+
+            //Opción2 
+            int num = 1;
+            do
+            {
+                if ((num % 2) == 0)
+                {
+                    Console.WriteLine($"NÚMERO PAR: {num}");
+                }
+                num++;
+
+            } while (num <= 100);
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
